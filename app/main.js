1f5b3ae9873c6d175d6785fc11092e7533006366
Kester Tong <kestert@google.com>
2014-03-08 13:38:58 -0500

    Modifies IPython static JavaScript files to use Google Drive for file management

--- app/main.js
+++ app/main.js
@@ -1,8 +1,19 @@
 chrome.app.runtime.onLaunched.addListener(function(intentData) {
-	chrome.app.window.create('window.html', {
-		'bounds': {
-			'width': 800,
-			'height': 600
+	var driveRedirectUrlPrefix = 'http://127.0.0.1:8000/drive?state=';
+	console.log(intentData);
+	if (intentData.id === 'handle_drive_action') {
+		var url = intentData.url;
+		if (url.indexOf(driveRedirectUrlPrefix) == 0) {
+			var state = JSON.parse(decodeURIComponent(url.substr(driveRedirectUrlPrefix.length)));
+			console.log(state);
+			if (state.action === 'open') {
+				chrome.app.window.create('window.html?' + state.ids[0], {
+            		'bounds': {
+                		'width': 800,
+                		'height': 600
+            		}
+        		});
+			}
 		}
-	});
+	}
 });
