9b53d04297cafbf44853b228609f90a6f241f7fd
Kester Tong <kestert@google.com>
2014-05-07 13:49:47 -0400

    Removes all files to start v2 branch

--- server/v1/serve.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import BaseHTTPServer
-import SimpleHTTPServer
-
-server = BaseHTTPServer.HTTPServer(('127.0.0.1', 8000),
-                                   SimpleHTTPServer.SimpleHTTPRequestHandler)
-server.serve_forever()
