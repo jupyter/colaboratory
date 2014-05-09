e302626f84eee01d8fd1a2d97686159c41bb6990
Kester Tong <kestert@google.com>
2014-05-09 18:12:10 -0400

    Fixes install and js

--- install.sh
+++ install.sh
@@ -10,3 +10,15 @@ cp -r ../closure-library/closure/goog/* build/static/frontend/js/
 # Create deps.js
 ../closure-library/closure/bin/build/depswriter.py --root=../closure-library/closure/goog --root=static/frontend/js/ > build/static/frontend/js/deps.js
 
+# copy Google Closure css
+mkdir -p build/static/frontend/css
+cp -r ../closure-library/closure/goog/css/* build/static/frontend/css/
+
+# copy extern files into build directory
+cp -r extern/* build/static/
+
+# copy files built by the Google build system to build directory
+# (note, this files should be replaced with files that are built
+# directly from source.
+cp -r built_js/* build/static/frontend/js
+cp -r built_css/* build/static/frontend/css
