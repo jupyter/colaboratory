e585d0e18dca862d13e4bf2aaa759c5d49674aec
Kester Tong <kestert@google.com>
2014-05-10 14:51:01 -0400

    Use separate IPython profile

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Builds and installs the web app
 
-if [ -z "$IPYTHON_PROFILE_PATH" ]; then
-    echo "IPYTHON_PROFILE_PATH not set, using ~/.ipython/profile_default"
-    IPYTHON_PROFILE_PATH=~/.ipython/profile_default
-fi 
+# Build an IPython profile containing startup (Python) scripts, and
+# static content for front end.
+BUILD_DIR=build/profile_default
+
+STATIC_CONTENT_DIR=$BUILD_DIR/static
+
+mkdir -p $BUILD_DIR/
+mkdir -p $STATIC_CONTENT_DIR/
 
 # copy static files to build directory
-cp -r static build/
+cp -r static/* $STATIC_CONTENT_DIR/
 
 # copy Google Closure library to build directory
-cp -r ../closure-library/closure/goog/* build/static/frontend/js/
+cp -r ../closure-library/closure/goog/* $STATIC_CONTENT_DIR/frontend/js/
 
 # Create deps.js
-../closure-library/closure/bin/build/depswriter.py --root=../closure-library/closure/goog --root=static/frontend/js/ > build/static/frontend/js/deps.js
+../closure-library/closure/bin/build/depswriter.py --root=../closure-library/closure/goog --root=static/frontend/js/ > $STATIC_CONTENT_DIR/frontend/js/deps.js
 
 # copy Google Closure css
 mkdir -p build/static/frontend/css
-cp -r ../closure-library/closure/goog/css/* build/static/frontend/css/
+cp -r ../closure-library/closure/goog/css/* $STATIC_CONTENT_DIR/frontend/css/
 
 # copy extern files into build directory
-cp -r extern/* build/static/
+cp -r extern/* $STATIC_CONTENT_DIR/
 
 # copy files built by the Google build system to build directory
 # (note, this files should be replaced with files that are built
 # directly from source.
-cp -r built_js/* build/static/frontend/js
-cp -r built_css/* build/static/frontend/css
-
-# copy build subdirectories to IPython directories
-mkdir -p $IPYTHON_PROFILE_PATH/static
-cp -r build/static $IPYTHON_PROFILE_PATH/
+cp -r built_js/* $STATIC_CONTENT_DIR/frontend/js
+cp -r built_css/* $STATIC_CONTENT_DIR/frontend/css
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Runs IPython notebook with custom profile
+IPYTHONDIR=`pwd`/build ipython notebook
