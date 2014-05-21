4301f8de0c8ad2eb70256cc2674ae6310fbfed1c
Kester Tong <kestert@google.com>
2014-05-21 13:19:18 -0400

    Adds new files from Google repo

---
+++ static/frontend/js/preferences.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @fileoverview Description of this file.
+ *
+ */
+
+goog.provide('colab.Preferences');
+
+/**
+ * @constructor
+ * @struct
+ */
+colab.Preferences = function() {
+  this.showLineNumbers = true;
+  this.fontSize = 14;
+};
