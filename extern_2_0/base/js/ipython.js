1248aefd391dd07010dbd80db36c0a8050dc2717
Kester Tong <kestert@google.com>
2014-05-22 15:45:56 -0400

    Combines support for IPython 1.1 and 2.0 in a single branch

---
+++ extern_2_0/base/js/ipython.js
@@ -0,0 +1,33 @@
+/**
+ * Interface for IPython library.  This is needed to be compatible with
+ * both IPython 1.1 and IPython 2.0.  Note this file does not encapsulate
+ * all JavaScript dependencies on IPython, only the ones that are
+ * siginficantly different between IPython 1.1 and 2.0.
+ *
+ * This file contains the interface for IPython 1.1.  The install script
+ * copies the correct version of this copy of this file to ipython.js in
+ * the install directory.
+ */
+
+var IPythonInterface = {};
+
+IPythonInterface.version = "2.0"
+
+IPythonInterface.createCallbacks = function(reply, set_next_input,
+                                            input, clear_output, output){
+    return {
+        'shell' : {
+            'reply': reply,
+            'payload': {'set_next_input': set_next_input}
+        },
+        'input' : input,
+        'iopub' : {
+            'clear_output': clear_output,
+            'output': output
+        }
+    };
+}
+
+IPythonInterface.KERNEL_STARTED_EVENT = 'status_started.Kernel';
+
+IPythonInterface.KERNEL_URL_KEY = 'kernel_service_url';
