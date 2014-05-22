863ed7e1bc12772b21f921615d0d959745d6cd03
Kester Tong <kestert@google.com>
2014-05-22 16:26:02 -0400

    fixes install_v2.sh

--- install_v2.sh
+++ install_v2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Builds and installs the web app for IPython 2.0
 
+./install.sh
+
+BUILD_DIR=build/profile_default
+STATIC_CONTENT_DIR=$BUILD_DIR/static
+
 # copy IPython 2.0 override files
 cp -r extern_2_0/* $STATIC_CONTENT_DIR/
 
