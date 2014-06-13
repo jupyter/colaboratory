f28acbf302600fc4d1083c1a8b535e406bb26442
kayur <kayurkayur@gmail.com>
2014-06-13 14:14:01 -0400

    Update install_v2.sh

--- install_v2.sh
+++ install_v2.sh
@@ -7,5 +7,6 @@ BUILD_DIR=build/profile_default
 STATIC_CONTENT_DIR=$BUILD_DIR/static
 
 # copy IPython 2.0 override files
+echo [coLaboratory Install] Copying IPython "2.0" override files
 cp -r --force extern_2_1/* $STATIC_CONTENT_DIR/
 
