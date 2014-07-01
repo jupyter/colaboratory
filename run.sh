a3d7350419be1e9851181e4260be35706a9474f2
MinRK <benjaminrk@gmail.com>
2014-06-30 17:10:28 -0700

    remove obsolete install.sh and run.sh
    update README and install_chrome with new resources location

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Runs IPython notebook with custom profile
-export FRONTENDMODULES=`pwd`/modules
-export PYTHONPATH=$FRONTENDMODULES:$PYTHONPATH
-export IPYTHONDIR=`pwd`/build
-
-echo [coLaboratory Run Script] Running IPython Notebook with the following args: $*
-ipython notebook $*
