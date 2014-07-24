a0b046cd0b9911aa709701015807a7ce429f931b
Matthew Turk <matthewturk@gmail.com>
2014-07-24 16:16:07 -0500

    Changing to use versioned kernels.

--- install_chrome.py
+++ install_chrome.py
@@ -19,8 +19,10 @@ args = parser.parse_args()
 
 RELEASE_CLIENT_ID = "911569945122-0tcrnl8lnu5b0ccgpp92al27pplahn5a.apps.googleusercontent.com"
 
-NACL_PEXE_FILE_URL = "http://yt-project.org/upload/kernel.pexe"
-NACL_TAR_FILE_URL = "http://yt-project.org/upload/zeropy_20140520.tar.gz"
+RELEASE = "20140722"
+base_url = "http://yt-project.org/files/colaboratory/%s/" % RELEASE
+NACL_PEXE_FILE_URL = base_url + "kernel.pexe"
+NACL_TAR_FILE_URL = base_url + "pnacl_data.tar.gz"
 
 
 
@@ -45,7 +47,7 @@ def InstallChrome(release, colab_root, dest):
   # Download .pexe and .tar.gz files.  Later these will be pulled
   # from the naclports continuous builder
   pexe_file = pjoin(dest, 'pnacl', 'kernel.pexe')
-  tar_file = pjoin(dest, 'zeropy_20140520.tar.gz')
+  tar_file = pjoin(dest, 'pnacl_data.tar.gz')
   url_opener = urllib.URLopener()
   if not os.path.isfile(pexe_file):
     print 'Downloading ' + NACL_PEXE_FILE_URL
